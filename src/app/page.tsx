99f5701ef489a43021a942644643c2bb696908a5
Shahazad <shanudjn@gmail.com>
2024-04-22 12:40:10 +0530

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/1481dbc7-9aa7-4daf-9f9e-95e83a63c0e6-3hj90w.jpg",
@@ -9,15 +9,20 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url: url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
-            <div key={image.id} className="h-full">
-              <img src={image.url} className="h-full w-full" />
-            </div>
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
+            <img src={image.url} className="h-full w-full" />
           </div>
         ))}
       </div>
